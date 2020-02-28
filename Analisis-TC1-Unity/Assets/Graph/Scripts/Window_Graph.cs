d5a9f745d5aab60377a961dbcdbb8de30606b5d3
Irsacfac <irsacfac@gmail.com>
2020-02-28 12:11:22 -0600

    Los puntos se conectan correctamente

--- Analisis-TC1-Unity/Assets/Graph/Scripts/Window_Graph.cs
+++ Analisis-TC1-Unity/Assets/Graph/Scripts/Window_Graph.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using CodeMonkey.Utils;
 
 public class Window_Graph : MonoBehaviour
 {
@@ -58,11 +59,15 @@ public class Window_Graph : MonoBehaviour
     private void CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB){
         GameObject gameObject = new GameObject("dotConnection", typeof(Image));
         gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().color = new Color(1,1,1, .5f);
         RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
         rectTransform.anchorMin = new Vector2(0, 0);
         rectTransform.anchorMax = new Vector2(0, 0);
-        rectTransform.sizeDelta = new Vector2(100, 3f);
-        rectTransform.anchoredPosition = dotPositionA;
+        rectTransform.sizeDelta = new Vector2(distance, 3f);
+        rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
+        rectTransform.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(dir));
     }
 
 }
